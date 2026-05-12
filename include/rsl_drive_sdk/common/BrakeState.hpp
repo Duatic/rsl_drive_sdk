d1e9b0a0f484a1b3d5098fdf3332f67b2dbfb8e7
Lennart Nachtigall <mail@firesurfer.de>
2026-05-12 08:34:17 +0200

    added ostream operator for BrakeState

--- include/rsl_drive_sdk/common/BrakeState.hpp
+++ include/rsl_drive_sdk/common/BrakeState.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <ostream>
 namespace rsl_drive_sdk
 {
     enum class BrakeState
@@ -8,4 +8,21 @@ namespace rsl_drive_sdk
         Excited = 1,
         Holding = 2
     };
+    inline std::ostream &operator<<(std::ostream &os, BrakeState state)
+    {
+        switch (state)
+        {
+        case BrakeState::Engaged:
+            return os << "Engaged";
+
+        case BrakeState::Excited:
+            return os << "Excited";
+
+        case BrakeState::Holding:
+            return os << "Holding";
+
+        default:
+            return os << "Unknown";
+        }
+    }
 }
